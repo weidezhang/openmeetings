2c594ad121639128e1901c1663841c813d1ba79f
Sebastian Wagner <sebawagner@apache.org>
2012-08-04 13:25:45 +0000

    OPENMEETINGS-389 initial version using REST API
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/trunk@1369316 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plugins/drupal/drupal_7.x/sites/all/modules/openmeetings/openmeetingsRoomManagament.php
@@ -0,0 +1,98 @@
+<?php
+
+
+require_once("openmeetings_gateway.php");
+
+
+class openmeetingsRoomManagament {
+
+
+	function createMyRoomWithMod($data) {
+		
+		$openmeetings_gateway = new openmeetings_gateway();
+		if ($openmeetings_gateway->openmeetings_loginuser()) {
+			
+			$openmeetings = new stdClass;
+			
+			$openmeetings->name = $data['name'];
+			$openmeetings->roomtypes_id = $data['roomtype_id'];
+			$openmeetings->comment = $data['comment'];
+			$openmeetings->numberOfPartizipants = $data['number_of_partizipants'];
+			$openmeetings->ispublic = $data['is_public'];
+			$openmeetings->isAudioOnly = $data['isAudioOnly'];
+			$openmeetings->allowUserQuestions = $data['allowUserQuestions'];
+			$openmeetings->isDemoRoom = 0;
+			$openmeetings->demoTime = "";
+			$openmeetings->isModeratedRoom = $data['is_moderated_room'];
+								
+			$roomid = $openmeetings_gateway->openmeetings_createRoomWithModAndTypeAndAudioOption($openmeetings);	
+				
+			return $roomid;
+						
+		} else {
+			echo "Could not login User to OpenMeetings, check your OpenMeetings Module Configuration [4]";			
+		}
+	}
+	
+	function updateRoomWithModerationAndQuestions($data) {
+		
+
+		$openmeetings_gateway = new openmeetings_gateway();
+		if ($openmeetings_gateway->openmeetings_loginuser()) {
+			
+			$openmeetings = new stdClass;
+			
+			$openmeetings->name = $data['name'];
+			$openmeetings->room_id = $data['room_id'];
+			$openmeetings->roomtypes_id = $data['roomtype_id'];
+			$openmeetings->comment = $data['comment'];
+			$openmeetings->numberOfPartizipants = $data['number_of_partizipants'];
+			$openmeetings->ispublic = $data['is_public'];
+			$openmeetings->appointment = $data['appointment'];
+			$openmeetings->isDemoRoom = 0;
+			$openmeetings->demoTime = "";
+			$openmeetings->isModeratedRoom = $data['is_moderated_room'];
+			$openmeetings->allowUserQuestions = $data['allowUserQuestions'];
+			
+			$roomid = $openmeetings_gateway->updateRoomWithModerationAndQuestions($openmeetings);	
+			
+			//echo "roomid". $roomid;
+			//exit();
+			
+				
+			return $roomid;
+						
+		} else {
+			echo "Could not login User to OpenMeetings, check your OpenMeetings Module Configuration [7]";
+			
+		}
+	}
+	
+	function deleteRoom ($data){
+
+		$openmeetings_gateway = new openmeetings_gateway();
+		if ($openmeetings_gateway->openmeetings_loginuser()) {
+			
+			$openmeetings = new stdClass;
+			
+			$openmeetings->room_id = $data['room_id'];	
+			if ($openmeetings->room_id > 0) {
+				$roomid = $openmeetings_gateway->deleteRoom($openmeetings);					
+				return $roomid;
+			} else {
+				return -1;
+			}
+						
+		} else {
+			echo "Could not login User to OpenMeetings, check your OpenMeetings Module Configuration [8]";
+			
+		}
+
+
+	}
+
+
+}
+
+
+?>
---
+++ plugins/drupal/drupal_7.x/sites/all/modules/openmeetings/openmeetings_conference.php
@@ -0,0 +1,15 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"></head>
+<body bgcolor="#5a5152" text="#333333" link="#FF3366" LEFTMARGIN="0" TOPMARGIN="0" MARGINWIDTH="0" MARGINHEIGHT="0">
+<?
+
+
+$swfurl = $_POST["swfurl"];
+
+?>
+<iframe src="<?=$swfurl?>" width="100%" height="100%">
+<p align="center"><strong>This content requires the Adobe Flash Player:
+  	<a href="http://www.macromedia.com/go/getflash/">Get Flash</a></strong>!</p>
+</iframe>
+<?
